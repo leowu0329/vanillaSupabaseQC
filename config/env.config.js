3a10e2f18ec4c96fe6c46c59c5831b77583edbde
leowu0329 <ryowuandjanet@gmail.com>
2026-06-27 02:53:17 +0800

    20260627-3

---
+++ config/env.config.js
@@ -0,0 +1,5 @@
+// config/env.config.js
+window.ENV = {
+    SUPABASE_URL: "https://uhpztucfioqrdnlfeyxw.supabase.co",
+    SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVocHp0dWNmaW9xcmRubGZleXh3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTgzNDc4MjksImV4cCI6MjA3MzkyMzgyOX0.3DA16OAzhXBpLxclMNee6deY_a6Z3qbSZyLVnX69NOU"
+};
